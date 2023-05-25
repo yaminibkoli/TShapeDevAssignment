1787425219181bf590c48f9fa6f0571b1801a5aa
yaminibkoli <62368463+yaminibkoli@users.noreply.github.com>
2023-05-25 20:08:17 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  
+  <DisplayTreeData/>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import DisplayTreeData from './components/DisplayTreeData.vue'
+export default {
+  name: 'App',
+ 
+  components: {
+    //  HelloWorld,
+    DisplayTreeData
+  },
+ 
+
+}
+</script>
+
+<style>
+#app {
+/*   
+ display: flex;
+ align-items: center; */
+
+}
+</style>
