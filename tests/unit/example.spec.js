d66744091e2e4f325532347ececa2e6c8cb3bd15
yaminibkoli <62368463+yaminibkoli@users.noreply.github.com>
2023-05-26 17:02:52 +0530

    Update example.spec.js

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -4,27 +4,20 @@ import DisplayTreeData from '@/components/DisplayTreeData.vue'
 describe('DisplayTreeData.vue',  () => {
   const wrapper = mount(DisplayTreeData)
   it('It should load the component', () => {
-   
-    expect(wrapper.exists()).toBeTruthy(); // component exists - test case 1
-    expect(wrapper.vm).toBeDefined(); // it  data and functions can be called- test case 1
+    expect(wrapper.exists()).toBeTruthy(); 
+    expect(wrapper.vm).toBeDefined(); 
   });
    it('tree data is not empty', () => {
-    // console.log("text###################",wrapper.vm.convertedTreeData);
-    expect(wrapper.vm.convertedTreeData.length).toBeGreaterThan(0)
+      expect(wrapper.vm.convertedTreeData.length).toBeGreaterThan(0)
   });
-  it('tree node click should open close data ', () => {
-    // console.log("text###################",wrapper.vm.convertedTreeData);
+  it('tree node click should open close dialog ', () => {
     expect(wrapper.vm.isNodeClose).toBe(false);
     wrapper.find('blocks-tree').trigger('node-click');
     expect(wrapper.vm.isNodeClose).toBe(true);
   });
-  it('dialog close', () => {
-    // console.log("text###################",wrapper.vm.convertedTreeData);
-    expect(wrapper.vm.isNodeClose).toBe(true);
-    
+  it('dialog open/close', () => {
+    expect(wrapper.vm.isNodeClose).toBe(true);   
     wrapper.find('button').trigger('click');
-    //wrapper.trigger('nodeCloseClicked');
-    //  wrapper.vm.NodecloseCliked();
     expect(wrapper.vm.isNodeClose).toBe(false);
   });
   
